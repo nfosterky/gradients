0325b5f3f5bad41142e91a3e051612adc8b4b2f7
Nathaniel Foster <nathanielsmusic@gmail.com>
2014-04-27 16:18:35 -0400

    organized code, more object oriented approach.  Adding multiple layers breaks the save to cache.

--- js/gradients.js
+++ js/gradients.js
@@ -14,29 +14,39 @@
 //
 //
 // }
-
+var Color = function(val, percentage) {
+	var color = {
+		val : val,
+		percentage : {
+			val : percentage,
+			plus : function () {
+				this.val++;
+			},
+			minus : function () {
+				this.val--;
+			}
+		}
+	}
+	return color;
+};
 angular.module('app', ['ngTouch']);
 var cache = window.localStorage ? window.localStorage : {};
-var LAYER1_COLORS = [{
-						val: "rgba(50,50,255,1)",
-						percentage: 50
-					},{
-						val: "rgba(255,50,50,1)",
-						percentage: 51
-					}],
-	LAYER2_COLORS = [{
-						val: "rgba(74, 255, 74, 0.3)",
-						percentage: 50
-					},{
-						val: "rgba(246, 255, 0, 0.5)",
-						percentage: 51
-					}];
+var LAYER1_COLORS = [
+			new Color ("rgba(50,50,255,1)", 50),
+			new Color ("rgba(255,50,50,1)", 51)
+		],
+		LAYER2_COLORS = [
+			new Color ("rgba(74, 255, 74, 0.3)", 50),
+		  new Color ("rgba(246, 255, 0, 0.5)", 51)
+		];
+
+
 $(document).ready(function (e) {
 	document.getElementById("control-panel").style.display = "block";
 });
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function rand_color() {
@@ -44,27 +54,42 @@ function rand_color() {
 		max = 255,
 		color = {
 		r: getRandomInt(min, max),
- 		g: getRandomInt(min, max),
- 		b: getRandomInt(min, max),
- 		o: Math.random().toFixed(2)
+		g: getRandomInt(min, max),
+		b: getRandomInt(min, max),
+		o: Math.random().toFixed(2)
 	}
 
 	return color;
 };
 
 var Layer = function(id, colors, angle) {
-	var color1 = rand_color(),
-		color2 = rand_color();
 	var layer = {
 		id: id,
 		gradientDirection: "linear",
-		angle: angle, //getRandomInt(0,360),
+		angle: {
+			val: angle, //getRandomInt(0,360),
+			plus: function () {
+				this.val++;
+			},
+			minus: function() {
+				this.val--;
+			}
+		},
 		isPercentage: false,
 		isRepeating: false,
 		isRadial: false,
 		size: 30,
 		selected: true,
-		colors: colors
+		// colors: colors,
+		color: {
+			list: colors,
+			add: function () {
+				this.list.push({val: "#ff0000", percentage: 100});
+			},
+			remove: function () {
+					this.list.splice(index, 1);
+			}
+		}
 	}
 
 	return layer;
@@ -73,32 +98,32 @@ var Layer = function(id, colors, angle) {
 var app = angular.module('app', []);
 
 app.directive('uiColorpicker', function() {
-    return {
-        restrict: 'E',
-        require: 'ngModel',
-        scope: false,
-        replace: true,
-        template: "<span><input type='color' class='input-small' /></span>",
-        link: function(scope, element, attrs, ngModel) {
-            var input = element.find('input');
-            var options = angular.extend({
-                color: ngModel.$viewValue,
-                move: function(color) {
-                    scope.$apply(function() {
-                      ngModel.$setViewValue(color.toRgbString());
-                    });
-                },
-				showAlpha: true,
-                clickoutFiresChange: true
-            }, scope.$eval(attrs.options));
+		return {
+				restrict: 'E',
+				require: 'ngModel',
+				scope: false,
+				replace: true,
+				template: "<span><input type='color' class='input-small' /></span>",
+				link: function(scope, element, attrs, ngModel) {
+						var input = element.find('input');
+						var options = angular.extend({
+								color: ngModel.$viewValue,
+								move: function(color) {
+										scope.$apply(function() {
+											ngModel.$setViewValue(color.toRgbString());
+										});
+								},
+								showAlpha: true,
+								clickoutFiresChange: true
+						}, scope.$eval(attrs.options));
 
-            ngModel.$render = function() {
-              input.spectrum('set', ngModel.$viewValue || '');
-            };
+						ngModel.$render = function() {
+							input.spectrum('set', ngModel.$viewValue || '');
+						};
 
-            input.spectrum(options);
-        }
-    };
+						input.spectrum(options);
+				}
+		};
 });
 
 app.controller('ColorCtrl', function($scope) {
@@ -110,15 +135,10 @@ app.controller('ColorCtrl', function($scope) {
 	$scope.currentIndex = 0;
 
 	$scope.saveToCache = function() {
-		var options = {
-			isPercentage:	   $scope.isPercentage,
-			isRepeating:	   $scope.isRepeating,
-			isRadial: 		   $scope.isRadial,
-			size: 			   $scope.size,
-			colors: 		   $scope.colors,
-			gradientDirection: $scope.gradientDirection
-		};
-		cache["savedImage"] = JSON.stringify(options);
+		cache["saved"] = JSON.stringify({
+			list: $scope.layer.list,
+			size: $scope.size
+		});
 	};
 
 	// 	control open/close gradient ui
@@ -138,94 +158,89 @@ app.controller('ColorCtrl', function($scope) {
 		$scope.displayToggle = name;
 	});
 
-	$scope.layers = [new Layer(0, LAYER1_COLORS, 135)];
-	$scope.layer = $scope.layers[0];
+	$scope.layer = {
+		index: 0,
+		list: [new Layer(0, LAYER1_COLORS, 135)],
+		current: {},
+		add : function() {
+			var newIndex = this.list.length;
+
+			this.list[newIndex] = new Layer(newIndex, [
+															new Color ("rgba(74, 255, 74, 0.3)", 50),
+															new Color ("rgba(246, 255, 0, 0.5)", 51)
+														], 225);
+			this.change(newIndex);
+		},
+		remove : function() {
+			console.log(this.current.index);
+			var index = $scope.layer.current.index,
+				newIndex = (index === 0) ? 0 : index - 1;
+
+			this.list.splice(index, 1);
+			this.change(newIndex);
+		},
+		change : function(index) {
+			this.current = $scope.layer.list[index];
+			this.current.index = index;
+		}
+	};
+
+	$scope.init = function () {
+		if (cache["saved"] &&
+				cache["saved"].length > 0 ) {
+					console.log(cache["saved"]);
+					var saved = JSON.parse(cache["saved"]);
+					$scope.layer.list = saved.list;
+					$scope.size = saved.size;
+				}
+
+		$scope.layer.current = $scope.layer.list[0];
+	};
 
 	// css for layer index
 	$scope.indexCss = function () {
 		// do something to show the selected layer.
 	}
 
-	$scope.addLayer = function () {
-		var newIndex = $scope.layers.length;
-
-		$scope.layers[newIndex] = new Layer(newIndex, LAYER2_COLORS, 225);
-		$scope.changeLayer(newIndex);
+	$scope.size = {
+		val: 50,
+		isPercentage: false,
+		measurement: "px",
+		plus: function () {
+				this.val++;
+		},
+		minus: function() {
+				this.val--;
+		}
 	}
-
-	$scope.removeLayer = function() {
-		console.log($scope.currentIndex);
-		var index = $scope.currentIndex,
-			newIndex = (index === 0) ? 0 : index - 1;
-
-
-		$scope.layers.splice(index, 1);
-		$scope.changeLayer(newIndex);
-	};
-
-	$scope.changeLayer = function(index) {
-		$scope.layer = $scope.layers[index];
-		$scope.currentIndex = index;
-	}
-
-	// $scope.init = function() {
-// 		// check for saved image;
-// 		if ( cache["savedImage"] &&
-// 			 cache["savedImage"].length > 0 ) {
-// 			var options = JSON.parse(cache["savedImage"]);
-// 			$scope.gradientDirection = options.gradientDirection;
-// 			$scope.isPercentage		 = options.isPercentage;
-// 			$scope.isRepeating		 = options.isRepeating;
-// 			$scope.isRadial			 = options.isRadial;
-// 			$scope.colors 			 = options.colors;
-// 			$scope.size 			 = options.size;
-// 		} else {
-// 			$scope.gradientDirection = "radial";
-// 			$scope.isPercentage = false;
-// 			$scope.isRepeating = true;
-// 			$scope.isRadial = true;
-// 			$scope.colors = [
-// 				{val: "#7ae2ff", percentage: 50},
-// 				{val: "#000077", percentage: 51}
-// 			];
-// 			$scope.size = 34;
-// 		}
-// 	};
 
 	// 	size measurement is percentage or px
 	$scope.$watch(function() {
-		$scope.layer.measurement = ($scope.isPercentage) ? "%" : "px";
-		return	$scope.layer.size + $scope.layer.measurement + " " + $scope.layer.size + $scope.layer.measurement + ";";
+		var s = $scope.size;
+		s.measurement = (s.isPercentage) ? "%" : "px";
+		return	s.val + s.measurement + " " + s.val + s.measurement + ";";
 	},function(opt) {
 		$scope.bgSize = opt;
 		$scope.saveToCache();
 	});
 
 	$scope.$watch(function() {
-		$scope.layer.gradientDirection = ($scope.layer.isRadial) ? "Radial" : "Linear";
-		$scope.iconDirection = ($scope.layer.isRadial) ? "icon-radio-checked" : "icon-menu";
-		$scope.displayAngle  = ($scope.layer.isRadial) ? "none" : "block";
-		return $scope.layer.gradientDirection;
+		var l = $scope.layer.current;
+		l.gradientDirection  = (l.isRadial) ? "Radial" : "Linear";
+		$scope.iconDirection = (l.isRadial) ? "icon-radio-checked" : "icon-menu";
+		$scope.displayAngle  = (l.isRadial) ? "none" : "block";
+		return l.gradientDirection;
 	},function(name) {
+		console.log(name);
 		$scope.nameDirection = name;
 	});
 
 	// display: btn isRepeating
 	$scope.$watch(function() {
-		return ($scope.layer.isRepeating) ? "Repeating - On" : "Repeating - Off";
+		return ($scope.layer.current.isRepeating) ? "Repeating - On" : "Repeating - Off";
 	},function(name) {
 		$scope.nameRepeating = name;
 	});
-
-	// info: add color to layer
-	$scope.addColor = function() {
-	 	$scope.layer.colors.push({val: "#ff0000", percentage: 100});
-	 };
-
-	// info: remove color from layer
-	$scope.removeColor = function(index) {
-		$scope.layer.colors.splice(index, 1);
-	};
 
 	function createGradient(colors) {
 		var color = ""
@@ -235,7 +250,7 @@ app.controller('ColorCtrl', function($scope) {
 		len = colors.length;
 		for (var i=0; i<len; i++) {
 			color = colors[i];
-			strColor = color.val + " " + color.percentage + "%";
+			strColor = color.val + " " + color.percentage.val + "%";
 			if (i+1!==len) strColor += ","
 			strLstColors += strColor;
 		}
@@ -259,7 +274,7 @@ app.controller('ColorCtrl', function($scope) {
 			bg[i] = (bg[i]) ? bg[i] + gradients[i] : gradients[i];
 			if(lyr.isRepeating) bg[i] += "repeating-";
 			bg[i] += lyr.gradientDirection + "-gradient( ";
-			if (lyr.gradientDirection.toLowerCase() === "linear") bg[i] += lyr.angle + "deg, ";
+			if (lyr.gradientDirection.toLowerCase() === "linear") bg[i] += lyr.angle.val + "deg, ";
 			bg[i] += strLstColors+")";
 			if (h !== 0) bg[i] += ",";
 			else bg[i] += ";";
@@ -272,13 +287,13 @@ app.controller('ColorCtrl', function($scope) {
 	// display: create gradient from layer and add to display
 	$scope.$watch(function() {
 		var color = ""
-	 		strColor = "",
-	 		strLstColors = "",
-	 		len = 0,
-	 		prefixLayer = [],
-	 		bg = [],
-	 		gradients = [
-	 				//"-webkit-gradient(",
+			strColor = "",
+			strLstColors = "",
+			len = 0,
+			prefixLayer = [],
+			bg = [],
+			gradients = [
+					//"-webkit-gradient(",
 					"-moz-",
 					"-webkit-",
 					"-o-",
@@ -287,19 +302,19 @@ app.controller('ColorCtrl', function($scope) {
 			];
 
 		var lyr = {};
-		for (var h=$scope.layers.length-1; h>=0; h--) {
-			lyr = $scope.layers[h];
-			strLstColors = createGradient(lyr.colors);
+		for (var h=$scope.layer.list.length-1; h>=0; h--) {
+			lyr = $scope.layer.list[h];
+			strLstColors = createGradient(lyr.color.list);
 
 			// loop through different browser css prefixes
 			// len = gradients.length;
- 			var l = "";
+			var l = "";
 			for (var i=0; i<len; i++) {
 
 				l = gradients[i];
 				if(lyr.isRepeating) l += "repeating-";
 				l += lyr.gradientDirection + "-gradient( ";
-				if (lyr.gradientDirection.toLowerCase() === "linear") l += lyr.angle + "deg, ";
+				if (lyr.gradientDirection.toLowerCase() === "linear") l += lyr.angle.val + "deg, ";
 				l += strLstColors+")";
 				prefixLayer[i] = l + ";";
 				if (h !== 0) l += ",";
@@ -307,44 +322,14 @@ app.controller('ColorCtrl', function($scope) {
 
 				bg[i] = (bg[i]) ? bg[i] + l : l;
 			}
-			$scope.layerGradients[h] = prefixLayer.join("background: ");
+			$scope.layer.list[h].gradient = prefixLayer.join("background: ");
 		}
 		$scope.cssBg = bg;
 		return bg.join("background: ");
 	}, function(bgImage) {
-	 	$scope.bgImage = bgImage;
-	 	$scope.saveToCache();
+		$scope.bgImage = bgImage;
+		$scope.saveToCache();
 	});
-
-
-
-
-	// info: change size
-	$scope.sizeMinus = function() {
-		$scope.layer.size--;
-	}
-
-	// info: change size
-	$scope.sizePlus = function() {
-		$scope.layer.size++;
-	}
-
-	// info:
-	$scope.positionPlus = function(index) {
-		$scope.layer.colors[index].percentage++;
-	}
-
-	// info
-	$scope.positionMinus = function(index) {
-		$scope.layer.colors[index].percentage--;
-	}
-
-	$scope.anglePlus = function(index) {
-		$scope.layer.angle++;
-	}
-	$scope.angleMinus = function(index) {
-		$scope.layer.angle--;
-	}
 
 	$scope.showCSS = function() {
 		var css =	"background: " + this.bgImage +
